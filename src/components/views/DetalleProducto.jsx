abddb133a0210fbb346b589cf78da057e4ca63ba
¨mago¨ <martingramajo08@gmail.com>
2023-08-17 19:53:17 -0300

    agrego los views para empezar a trabajar cuando se designen a cada miembro

---
+++ src/components/views/DetalleProducto.jsx
@@ -0,0 +1,9 @@
+const DetalleProducto = () => {
+  return (
+    <div>
+      <h1>DetalleProducto</h1>
+    </div>
+  );
+};
+
+export default DetalleProducto;
---
+++ src/helpers/queries.js
@@ -0,0 +1,2 @@
+// aqui vamos a tener las funciones comunes para hacer las distintas 
+// peticiones al JSON-server
