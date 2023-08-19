fc8d45b55c04e3eb7eb4e5ad9132f9801fc2538e
¨mago¨ <martingramajo08@gmail.com>
2023-08-19 18:37:12 -0300

    agrego navbar y el react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MenuReact from "./components/common/MenuReact";
+import Inicio from "./components/views/Inicio";
+import DetalleProducto from "./components/views/DetalleProducto";
+import Administrador from "./components/views/Administrador";
+import Login from "./components/views/Login";
+import Nosotros from "./components/views/Nosotros";
 
 function App() {
   return (
     <>
-      <div>hello world</div>
+      <BrowserRouter>
+        <MenuReact />
+        <Routes>
+          <Route exact path="/" element={<Inicio />} />
+          <Route exact path="/detalle" element={<DetalleProducto />} />
+          <Route exact path="/administrador" element={<Administrador />} />
+          <Route exact path="/nosotros" element={<Nosotros />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/common/MenuReact.jsx
+++ src/components/common/MenuReact.jsx
@@ -1,8 +1,32 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link, NavLink } from "react-router-dom";
+
 const MenuReact = () => {
   return (
-    <div>
-      <h1>MenuReact</h1>
-    </div>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link}>logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavLink className="nav-link" end to={"/"}>
+              Inicio
+            </NavLink>
+            <NavLink className="nav-link" end to="/Nosotros">
+              Acerca de nosotros
+            </NavLink>
+            <NavLink className="nav-link" end to="/Administrador">
+              Administrador
+            </NavLink>
+            <NavLink className="nav-link" end to="/Login">
+              Login
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 };
 
