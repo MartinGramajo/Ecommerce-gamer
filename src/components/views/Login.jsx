c09a1fa8a00611192f65906c9c2b0aca0d39be47
[LucasA] <[lucas.anachuri@gmail.com]>
2023-08-20 20:30:16 -0300

    agrego formulario de login

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,8 +1,31 @@
+import { Container } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Container>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label className="text-">Ingrese correo electronico</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="ejemplo@gmail.com"
+          ></Form.Control>
+          <Form.Text className="text-muted"></Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Ingrese contraseña</Form.Label>
+          <Form.Control type="password" placeholder="Contraseña"></Form.Control>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Mantener conectado"></Form.Check>
+          </Form.Group>
+        </Form.Group>
+        <Button variant="secondary" type="submit">
+          Enviar
+        </Button>
+      </Form>
+    </Container>
   );
 };
 
