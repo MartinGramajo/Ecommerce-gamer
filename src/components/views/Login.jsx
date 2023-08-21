3a0020682fcfbbd5f825a306040b3ae952ed832e
[LucasA] <[lucas.anachuri@gmail.com]>
2023-08-20 21:02:47 -0300

    se agrega maquetado de login

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -6,21 +6,28 @@ const Login = () => {
   return (
     <Container>
       <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label className="text-">Ingrese correo electronico</Form.Label>
+        <Form.Group className="mb-4" controlId="formBasicEmail">
+          <Form.Label className="text-">
+            Ingrese su correo electronico
+          </Form.Label>
           <Form.Control
             type="email"
             placeholder="ejemplo@gmail.com"
           ></Form.Control>
           <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Ingrese contraseña</Form.Label>
+        <Form.Group className="mb-2" controlId="formBasicPassword">
+          <Form.Label>Ingrese su contraseña</Form.Label>
           <Form.Control type="password" placeholder="Contraseña"></Form.Control>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Mantener conectado"></Form.Check>
-          </Form.Group>
+          <Form.Group
+            className="mb-1"
+            controlId="formBasicCheckbox"
+          ></Form.Group>
         </Form.Group>
+        <Button className="mb-2" variant="link">
+          Has olvidado la contraseña?
+        </Button>
+        <div></div>
         <Button variant="secondary" type="submit">
           Enviar
         </Button>
