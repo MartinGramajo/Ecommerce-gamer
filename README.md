ea3c8dcad8c177d09cdf1775ae60e4e3a7c165c9
[LucasA] <[lucas.anachuri@gmail.com]>
2023-08-26 20:35:18 -0300

    se agrega readme

--- README.md
+++ README.md
@@ -1,8 +1,42 @@
-# React + Vite
+## Proyecto Modulo 3 Rolling Code
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+**Proyecto de ecommerce gamer**
 
-Currently, two official plugins are available:
+<img src="./src/assets/logo.png">
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+## Tecnologies / Tools
+
+- HTML **v5**
+- CSS **v3**
+- [React](https://es.react.dev/)
+- [Node Js](https://nodejs.org/es)
+- [NPM](https://www.npmjs.com/)
+- [React Bootstrap](https://react-bootstrap.github.io/) **v2.9.0-beta.1**
+- [Trello](https://trello.com/b/4h4qvy8X/grupo-4-rcs)
+
+## Requirements
+
+- [Project requirements](https://drive.google.com/file/d/10pFRMiGUFXSEBanlQbjBNYhrjtpnz66e/view)
+- [Nodejs](https://nodejs.org/en/) **v18.17.1**
+- [Npm](https://www.npmjs.com/) **v9.8.1**
+
+## How to clone repositorie
+
+```bash
+git clone https://github.com/MartinGramajo/Ecommerce-gamer
+```
+
+## How to contribute the to project
+
+If you want to help with this project, please read our [Contributing Guide](CONTRIBUTING.md) before making any changes. Contact us at the following email: **admin@rolling.com**
+
+## Project authors
+
+- Martin Gramajo
+- Daniela Geronimo
+- Federico Iñigo
+- Lucas Anachuri
+
+## Licence
+
+Copyright (c) 2023. All rights reserved
--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,4 +1,4 @@
-import { Container, Button, Form } from "react-bootstrap";
+import { Container, Button, Form, Card } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
 import { login } from "../../helpers/queries";
@@ -55,54 +55,61 @@ const Login = ({ setUsuarioActivo }) => {
   };
 
   return (
-    <Container className="mainSection">
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Form.Group className="mb-4" controlId="formBasicEmail">
-          <Form.Label className="text-">
-            Ingrese su correo electronico
-          </Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="ejemplo@gmail.com"
-            {...register("email", {
-              required: "El campo email es obligatorio",
-              pattern: {
-                value:
-                  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
-                message:
-                  "El email debe cumplir con un formato valido como el siguiente ejemplo@gmail.com",
-              },
-            })}
-          />
-          <Form.Text className="text-muted">{errors.email?.message}</Form.Text>
-        </Form.Group>
+    <Container className="mainSection bgLogin">
+      <Card className="my-5 ">
+        <Card.Header as="h4">Cuenta Intendo</Card.Header>
+        <Card.Body>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className="mb-4" controlId="formBasicEmail">
+              <Form.Label className="text">
+                Ingrese su correo electronico
+              </Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="ejemplo@gmail.com"
+                {...register("email", {
+                  required: "El campo email es obligatorio",
+                  pattern: {
+                    value:
+                      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                    message:
+                      "El email debe cumplir con un formato valido como el siguiente ejemplo@gmail.com",
+                  },
+                })}
+              />
+              <Form.Text className="text-muted">
+                {errors.email?.message}
+              </Form.Text>
+            </Form.Group>
 
-        <Form.Group className="mb-2" controlId="formBasicPassword">
-          <Form.Label>Ingrese su contraseña</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Contraseña"
-            {...register("password", {
-              required: "Debe ingresar una contraseña",
-              pattern: {
-                value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
-                message:
-                  "La contraseña debe tener entre 8 y 16 caracteres, al menos un numero, una minuscula y una mayuscula",
-              },
-            })}
-          />
-          <Form.Text className="text-muted">
-            {errors.password?.message}
-          </Form.Text>
-        </Form.Group>
-        <Button className="mb-2" variant="link">
-          Has olvidado la contraseña?
-        </Button>
-        <div></div>
-        <Button variant="secondary" type="submit">
-          Enviar
-        </Button>
-      </Form>
+            <Form.Group className="mb-2" controlId="formBasicPassword">
+              <Form.Label>Ingrese su contraseña</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Contraseña"
+                {...register("password", {
+                  required: "Debe ingresar una contraseña",
+                  pattern: {
+                    value: /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                    message:
+                      "La contraseña debe tener entre 8 y 16 caracteres, al menos un numero, una minuscula y una mayuscula",
+                  },
+                })}
+              />
+              <Form.Text className="text-muted">
+                {errors.password?.message}
+              </Form.Text>
+            </Form.Group>
+            <Button className="mb-2" variant="link">
+              Has olvidado la contraseña?
+            </Button>
+            <div></div>
+            <Button variant="secondary" type="submit">
+              Enviar
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
     </Container>
   );
 };
